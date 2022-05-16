01b59bf934f22e334e61e084ca152e3b58c37efc
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-05-16 12:45:47 +0100

    Support stdin on the CLI

--- Sources/xmltodict/main.swift
+++ Sources/xmltodict/main.swift
@@ -1,12 +1,18 @@
 import Foundation
 import XMLDictionary
 
-precondition(ProcessInfo.processInfo.arguments.count == 2)
+let data: Data
+var args = ProcessInfo.processInfo.arguments.makeIterator()
+switch (args.next(), args.next()) {
+case (.some, nil), (.some, "-"):
+    data = FileHandle.standardInput.readDataToEndOfFile()
+case (.some, let path?):
+    data = try Data(contentsOf: URL(fileURLWithPath: (path as NSString).expandingTildeInPath))
+default:
+    fatalError()
+}
 
 var options = JSONSerialization.WritingOptions.prettyPrinted
-if #available(OSX 10.13, *) {
-    options.insert(.prettyPrinted)
-}
 if #available(OSX 10.15, *) {
     options.insert(.withoutEscapingSlashes)
 }
@@ -14,13 +20,7 @@ if #available(OSX 10.15, *) {
 print(
     String(
         data: try JSONSerialization.data(
-            withJSONObject: NSDictionary(
-                XML: Data(
-                    contentsOf: URL(
-                        fileURLWithPath: (ProcessInfo.processInfo.arguments[1] as NSString).expandingTildeInPath
-                    )
-                )
-            ),
+            withJSONObject: NSDictionary(XML: data),
             options: options
         ),
         encoding: .utf8
