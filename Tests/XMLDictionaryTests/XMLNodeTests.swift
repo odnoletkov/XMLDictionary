308df4d3c60d23ae0a795e298b560961d3e54eb5
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-11-24 16:58:15 +0000

    Revert "Switch to protocol"
    This reverts commit e126e402fff142837aab1417be931914f3787677.

--- Tests/XMLDictionaryTests/XMLNodeTests.swift
+++ Tests/XMLDictionaryTests/XMLNodeTests.swift
@@ -15,32 +15,32 @@ final class XMLNodeTests: XCTestCase {
             """.data(using: .utf8)!
         )
 
-        let root = try XML(dict).root
+        let root = try XMLNode(dict).root
 
         let nullNode = try root.null
         XCTAssertEqual(nullNode.text, "")
         XCTAssertNil(nullNode["attr"])
         XCTAssertEqual(nullNode.children("child").count, 0)
         XCTAssertThrowsError(try nullNode.child) {
-            XCTAssertEqual($0 as? Error, Error.missingChild("child"))
+            XCTAssertEqual($0 as? XMLNode.Error, XMLNode.Error.missingChild("child"))
         }
 
         let textNode = try root.childWithText
-        XCTAssertTrue(textNode is XMLStringNode)
+        XCTAssertTrue(textNode.value is String)
         XCTAssertEqual(textNode.text, "foo")
         XCTAssertNil(textNode["attr"])
         XCTAssertEqual(textNode.children("child").count, 0)
         XCTAssertThrowsError(try textNode.child) {
-            XCTAssertEqual($0 as? Error, Error.missingChild("child"))
+            XCTAssertEqual($0 as? XMLNode.Error, XMLNode.Error.missingChild("child"))
         }
 
         let dictionaryNode = try root.childWithoutText
-        XCTAssertTrue(dictionaryNode is XMLDictionaryNode)
+        XCTAssertTrue(dictionaryNode.value is NSDictionary)
         XCTAssertEqual(dictionaryNode.text, "")
         XCTAssertEqual(dictionaryNode["attr"], "value"); XCTAssertNil(dictionaryNode["non-existent"])
         XCTAssertEqual(dictionaryNode.children("child").count, 0)
         XCTAssertThrowsError(try dictionaryNode.child) {
-            XCTAssertEqual($0 as? Error, Error.missingChild("child"))
+            XCTAssertEqual($0 as? XMLNode.Error, XMLNode.Error.missingChild("child"))
         }
 
         XCTAssertEqual(root.children("null").count, 1)
@@ -50,17 +50,46 @@ final class XMLNodeTests: XCTestCase {
         XCTAssertEqual(root.children("nonExistent").count, 0)
 
         XCTAssertThrowsError(try root.child) {
-            XCTAssertEqual($0 as? Error, Error.multipleChildren("child"))
+            XCTAssertEqual($0 as? XMLNode.Error, XMLNode.Error.multipleChildren("child"))
         }
     }
 }
 
-@dynamicMemberLookup
-protocol XMLNode {
-    var denormalizedValue: NSDictionary { get }
-}
+//@dynamicMemberLookup
+//protocol Test: Equatable {
+//    subscript(dynamicMember singleChildName: StaticString) -> any Test { get }
+//}
 
-extension XMLNode {
+@dynamicMemberLookup
+struct XMLNode {
+
+    /// String or NSDictionary
+    let value: Any
+
+    init(_ string: String) {
+        self.value = string
+    }
+
+    init(_ dictionary: NSDictionary) {
+        self.value = dictionary
+    }
+
+    private init(_ value: Any) {
+        precondition(value is String || value is NSDictionary)
+        self.value = value
+    }
+
+    var denormalizedValue: NSDictionary {
+        switch value {
+        case let string as String:
+            return ["#text": string]
+        case let dictionary as NSDictionary:
+            return dictionary
+        default:
+            preconditionFailure()
+        }
+    }
+
     var text: String {
         denormalizedValue["#text"] as! String? ?? ""
     }
@@ -72,11 +101,11 @@ extension XMLNode {
     func children(_ name: StaticString) -> [XMLNode] {
         switch denormalizedValue[name.description] {
         case let array as NSArray:
-            return array.map(XML)
+            return array.map(XMLNode.init)
         case nil:
             return []
         case let value?:
-            return [XML(value)]
+            return [XMLNode(value)]
         }
     }
 
@@ -93,34 +122,9 @@ extension XMLNode {
             }
         }
     }
-}
 
-enum Error: Swift.Error, Equatable {
-    case missingChild(_ name: String)
-    case multipleChildren(_ name: String)
-}
-
-struct XMLStringNode: XMLNode {
-    let value: String
-    var denormalizedValue: NSDictionary {
-        ["#text": value]
-    }
-}
-
-struct XMLDictionaryNode: XMLNode {
-    let value: NSDictionary
-    var denormalizedValue: NSDictionary {
-        value
-    }
-}
-
-func XML(_ value: Any) -> XMLNode {
-    switch value {
-    case let string as String:
-        return XMLStringNode(value: string)
-    case let dictionary as NSDictionary:
-        return XMLDictionaryNode(value: dictionary)
-    default:
-        preconditionFailure()
+    enum Error: Swift.Error, Equatable {
+        case missingChild(_ name: String)
+        case multipleChildren(_ name: String)
     }
 }
